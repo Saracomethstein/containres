03ef88860fd306501ef0ef5d751454557e6ddc90
Baratheon Alaina <barathea@va-b5.kzn.21-school.ru>
2024-06-24 16:03:36 +0300

    add operators for iterators

--- list/list.h
+++ list/list.h
@@ -9,8 +9,8 @@
 //  [done] - list(const list &l);
 //  [done] - list(list &&l);
 //  [done] - operator=(list &&l);
-//  - const_reference front();
-//  - const_reference back();
+//  [done] - const_reference front();
+//  [done] - const_reference back();
 //  - iterator begin();
 //  - iterator end();
 //  - iterator insert(iterator pos, const_reference value);
@@ -213,14 +213,75 @@ class list {
   class ListIterator {
    public:
     ListIterator() { current_ = nullptr; }
+    explicit ListIterator(Node *current) : current_(current) {}
+
+    // Operators //
+    reference operator*() {
+      if (!current_) {
+        throw std::invalid_argument("Value is nullptr");
+      }
+      return current_->value_;
+    }
+
+    ListIterator operator++(int) {
+      current_ = current_->next_;
+      return *this;
+    }
+
+    ListIterator operator--(int) {
+      current_ = current_->prev_;
+      return *this;
+    }
+
+    ListIterator &operator++() {
+      current_ = current_->next_;
+      return *this;
+    }
+
+    ListIterator &operator--() {
+      current_ = current_->prev_;
+      return *this;
+    }
+
+    ListIterator operator+(const size_type value) {
+      Node *tmp = current_;
+      for (size_type i = 0; i < value; i++) {
+        tmp = tmp->next_;
+      }
+
+      ListIterator res(tmp);
+      return res;
+    }
+
+    ListIterator operator-(const size_type value) {
+      Node *tmp = current_;
+      for (size_type i = 0; i < value; i++) {
+        tmp = tmp->prev_;
+      }
+      ListIterator res(tmp);
+      return res;
+    }
+
+    bool operator==(ListIterator other) { return current_ == other.current_; }
+    bool operator!=(ListIterator other) { return current_ != other.current_; }
+    bool operator<(ListIterator other) { return current_ < other.current; }
+    bool operator<=(ListIterator other) { return current_ <= other.current_; }
+    bool operator>(ListIterator other) { return current_ > other.current_; }
+    bool operator>=(ListIterator other) { return current_ >= other.currnet_; }
 
    private:
     Node *current_ = nullptr;
     friend class list<value_type>;
   };
 
+  // Const Iterator //
   template <typename value_type>
-  class ListConstIterator {};
+  class ListConstIterator : ListIterator<T> {
+   public:
+    explicit ListConstIterator(ListIterator<T> other)
+        : ListIterator<T>(other) {}
+    const T &operator*() { return ListIterator<T>::operator*(); }
+  };
 
   //  List Member type
   using iterator = ListIterator<T>;
